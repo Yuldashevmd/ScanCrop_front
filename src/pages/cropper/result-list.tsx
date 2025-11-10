a21204dec58a141cefc04c5dd9c00b99bdc24439
yuldashev_md <muxammad_205@mail.ru>
2025-11-10 14:42:15 +0500

    feat(modal): modal to show image

--- src/pages/cropper/result-list.tsx
+++ src/pages/cropper/result-list.tsx
@@ -1,7 +1,22 @@
 import { DownloadImages } from 'features/download-images';
 import { type CroppedResult } from './index';
+import { useDisclosure } from 'shared/lib';
+import { useState } from 'react';
 
 export function ResultList({ results }: { results: CroppedResult[] }) {
+  const { onOpen, isOpen, onClose } = useDisclosure();
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const openImage = (url: string) => {
+    onOpen();
+    setImageUrl(url);
+  };
+
+  const closeImage = () => {
+    onClose();
+    setImageUrl(undefined);
+  };
+
   return (
     <>
       {results.length > 0 && (
@@ -11,11 +26,14 @@ export function ResultList({ results }: { results: CroppedResult[] }) {
               <div
                 key={r.id}
                 className="bg-white/10 backdrop-blur-md p-4 rounded-2xl flex flex-col items-center shadow-lg">
-                <img
-                  src={r.url}
-                  alt={r.name}
-                  className="rounded-xl w-full aspect-square object-cover border border-white/20"
-                />
+                <button onClick={() => openImage(r.url)}>
+                  <img
+                    loading="lazy"
+                    src={r.url}
+                    alt={r.name}
+                    className="rounded-xl w-full aspect-square object-cover border border-white/20"
+                  />
+                </button>
                 <SingleDownload r={r} />
               </div>
             ))}
@@ -23,6 +41,8 @@ export function ResultList({ results }: { results: CroppedResult[] }) {
           <DownloadImages results={results} />
         </div>
       )}
+      {/* Dialog */}
+      <Dialog isOpen={isOpen} imageUrl={imageUrl} closeImage={closeImage} />
     </>
   );
 }
@@ -74,5 +94,41 @@ export function SingleDownload({ r }: { r: CroppedResult }) {
       className="mt-3 w-full py-2 rounded-full bg-white text-indigo-600 font-semibold shadow-md hover:bg-indigo-50 transition">
       Save As...
     </button>
+  );
+}
+
+function Dialog({
+  imageUrl,
+  isOpen,
+  closeImage,
+}: {
+  imageUrl: string | undefined;
+  isOpen: boolean;
+  closeImage: () => void;
+}) {
+  if (!imageUrl) return null;
+
+  return (
+    <div
+      onClick={(e) => {
+        if (e.target === e.currentTarget) {
+          closeImage();
+        }
+      }}
+      className={
+        isOpen
+          ? 'fixed inset-0 w-full h-full flex items-center justify-center bg-black/50 z-50'
+          : 'hidden'
+      }>
+      <button className="absolute right-2 top-1 text-2xl text-white" onClick={closeImage}>
+        X
+      </button>
+      <img
+        loading="lazy"
+        src={imageUrl}
+        alt={'image'}
+        className="rounded-xl max-w-fit max-h-fit w-full h-full"
+      />
+    </div>
   );
 }
