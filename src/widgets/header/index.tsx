6afb143ef78d24dc9b0d7df69eb0b026538114bc
yuldashev_md <muxammad_205@mail.ru>
2025-11-12 13:01:00 +0500

    feat(Login): Login ready

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,4 +1,5 @@
 import { AuthProfile } from 'features/auth-profile';
+import { Link } from 'react-router';
 import { useLang } from 'shared/lib';
 
 export function HeaderUI() {
@@ -7,7 +8,9 @@ export function HeaderUI() {
   return (
     <header className="bg-[#337ab7] h-[50px] w-full px-4 flex justify-center items-center text-white">
       <section className="max-w-[1080px] w-full flex justify-between items-center">
-        <h2 className="text-xl font-semibold">{t('app-name')}</h2>
+        <Link to={'/'}>
+          <h2 className="text-xl font-semibold">{t('app-name')}</h2>
+        </Link>
 
         {/* AUTH */}
         <AuthProfile />
