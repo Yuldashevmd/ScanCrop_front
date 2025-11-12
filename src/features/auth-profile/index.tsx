7c8fac18969c947d463ae71b5fdb728132455096
yuldashev_md <muxammad_205@mail.ru>
2025-11-12 11:51:01 +0500

    feat(Language): Language

--- src/features/auth-profile/index.tsx
+++ src/features/auth-profile/index.tsx
@@ -1,8 +1,12 @@
+import { useLang } from 'shared/lib';
+
 export function AuthProfile() {
+  const { t } = useLang();
+
   return (
     <div>
       <button className="min-w-[100px] from-[#245580] to-[#337ab7] bg-gradient-to-t px-3 py-1 rounded text-center cursor-pointer">
-        Sign in
+        {t('login')}
       </button>
     </div>
   );
