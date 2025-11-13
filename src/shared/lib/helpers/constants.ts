2309016bddf25b90baa479437214b9eaf632564d
yuldashev_md <muxammad_205@mail.ru>
2025-11-13 22:02:56 +0500

    feat(Env): Prod and dev env

--- src/shared/lib/helpers/constants.ts
+++ src/shared/lib/helpers/constants.ts
@@ -1,4 +1,7 @@
-export const BASE_URL = import.meta.env.VITE_APP_URL_DEV;
+export const BASE_URL =
+  import.meta.env.VITE_APP_REACT === 'dev'
+    ? import.meta.env.VITE_APP_URL_DEV
+    : import.meta.env.VITE_APP_URL_PROD;
 
 export const API_MAP = {
   LOGIN: '/login',
